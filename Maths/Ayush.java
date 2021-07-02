7831e41d2becf46c88b00400e548404bdfc33f67
Ashish_Khati <khatiashish2442@gmail.com>
2021-07-02 23:39:50 +0530

    Third commit in AshishBranch

---
+++ Maths/Ayush.java
@@ -0,0 +1,11 @@
+package com.Maths;
+public  class Ayush
+{
+	public static void main(String args[])
+	{
+		System.out.println("This is Ayush Khati");
+		System.out.println("This is Ashish Khati");
+		System.out.println("Again from Ashish Branch");
+		
+	}
+}
