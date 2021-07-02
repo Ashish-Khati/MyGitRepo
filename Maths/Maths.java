369012d2406dac60aeba29fa4b8427ef54b3eabc
Ashish_Khati <khatiashish2442@gmail.com>
2021-07-02 20:28:11 +0530

    Initial commit

---
+++ Maths/Maths.java
@@ -0,0 +1,38 @@
+
+package com.Maths;
+import java.util.Scanner;
+
+public class Maths {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,sum;
+		Diff obj=new Diff();
+		obj.getData(100,20);
+		obj.show();
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter first number: ");
+		a=sc.nextInt();
+		System.out.print("Enter second number: ");
+		b=sc.nextInt();
+		sum=a+b;
+		System.out.print("Sum is :"+sum);
+		
+		
+
+	}
+
+}
+class Diff
+{
+	int a,b;
+	public  void getData(int a,int b)
+	{
+		this.a=a;
+		this.b=b;
+	}
+	public  void show()
+	{
+		System.out.println("Difference is :"+(a-b));
+	}
+}
